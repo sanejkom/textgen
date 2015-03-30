dc4c1e98a7a653fddcdf4fd2ac71cb7c937623ba
Mateusz Sanejko <sanejkom@len.iem.pw.edu.pl>
2015-03-30 23:04:20 +0200

    Wypełnianie drzewa

--- src/tree.c
+++ src/tree.c
@@ -9,9 +9,9 @@ struct dyn_tab * dyn_tab_init( int init_size )
         return NULL;
 	ns->t = malloc( init_size * sizeof * ns->t );
 	if( ns->t == NULL )
-    {
-        free( ns );
-        return NULL;
+	{
+        	free( ns );
+        	return NULL;
 	}
 	ns->size = init_size;
 	ns->n = 0;
@@ -21,8 +21,8 @@ struct dyn_tab * dyn_tab_init( int init_size )
 struct dyn_tab * dyn_tab_resize( struct dyn_tab *t )
 {
 	double *np = realloc( t->t, 2*t->size*sizeof *t->t );
-	if( np == NULL )
-		return NULL;
+	//if( np == NULL )
+	//	return NULL;
 	t->size *= 2;
 	t->t = np;
 	return t;
@@ -31,24 +31,22 @@ struct dyn_tab * dyn_tab_resize( struct dyn_tab *t )
 void dyn_tab_add( struct dyn_tab * sp, int x )
 {
 	if( sp->size <= sp->n )
-		if( dyn_tab_resize( sp ) == NULL )
-			return NULL;
-		sp->t[sp->n] = x;
-		sp->n++;
-		return NULL;
+		dyn_tab_resize( sp );
+	sp->t[sp->n] = x;
+	sp->n++;
 }
 
 struct str_tab * str_tab_init( int init_size )
 {
 	struct str_tab * ns = malloc( sizeof *ns );
-	if( ns == NULL )
-        return NULL;
+	//if( ns == NULL )
+        //	return NULL;
 	ns->t = malloc( init_size * sizeof * ns->t );
-	if( ns->t == NULL )
-    {
-        free( ns );
-        return NULL;
-	}
+	//if( ns->t == NULL )
+	//{
+        //	free( ns );
+        //	return NULL;
+	//}
 	ns->size = init_size;
 	ns->n = 0;
 	return ns;
@@ -56,7 +54,7 @@ struct str_tab * str_tab_init( int init_size )
 
 tree_t tree_add( tree_t t, char *pref, char *suf ) {
 	if( t == NULL )
-    {
+	{
 		node * n = malloc( sizeof *n );
 		n->prefiks = malloc( (strlen(pref) + 1) * sizeof *(n->prefiks) );
 		n->sufiks = str_tab_init( 10 );
@@ -67,36 +65,36 @@ tree_t tree_add( tree_t t, char *pref, char *suf ) {
 		n->left = n-> right = NULL;
 		return n;
 	}
-	else if( t->d > x ) //TODO
-    	{
-		
-	}
-	else if( t->d < x )  //TODO
+	else if( strcmp( t->prefiks, pref ) > 0 )
 	{
-		
+		t->left = insert( t->left, pref, suf );
+		return t;
 	}
-	else //TODO
+	else if( strcmp( t->prefiks, pref ) < 0 )
 	{
-		
+		t->right = insert( t->right, pref, suf );
+		return t;
+	} else {
+	    sufiks_add( t, suf );
+		return t;
 	}
 }
 
 void sufiks_add( node *n, char *suf )
 {
-    int i = 0;
-    while( strcmp( n->sufiks[i], suf) != 0 && i < n->wyst )
-        i++;
-    if( i < n->wyst )
-        n->liczba_suf[i]++;
-    else
-    {
-        if( n->sufiks->size <= n->sufiks->n )
-            if( dyn_tab_resize( n->sufiks ) == NULL )
-                return NULL;
-        n->sufiks->t[n->sufiks->n] = malloc( (strlen(suf) + 1) * sizeof *(n->sufiks->t[n->sufiks->n]) );
-        strcpy( n->sufiks->t[n->sufiks->n], suf);
-		n->sufiks->n++;
-		dyn_tab_add( n->liczba_suf, 1);
-		return NULL;
+	int i = 0;
+	while( strcmp( n->sufiks[i], suf) != 0 && i < n->wyst )
+		i++;
+	if( i < n->wyst )
+		n->liczba_suf[i]++;
+	else
+	{
+		if( n->sufiks->size <= n->sufiks->n )
+			if( dyn_tab_resize( n->sufiks ) == NULL )
+				return NULL;
+		n->sufiks->t[n->sufiks->n] = malloc( (strlen(suf) + 1) * sizeof *(n->sufiks->t[n->sufiks->n]) );
+		strcpy( n->sufiks->t[n->sufiks->n], suf);
+	n->sufiks->n++;
+	dyn_tab_add( n->liczba_suf, 1);
     }
 }
